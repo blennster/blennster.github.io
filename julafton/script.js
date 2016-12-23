0ebe611ce59738f49ba1442050faa5b3f577c5e3
blennster <mr.emil101@gmail.com>
2016-12-23 19:19:34 +0100

    Add files via upload

---
+++ julafton/script.js
@@ -0,0 +1,16 @@
+function submit() {
+  var namn = document.getElementById("nameInput").value;
+  var pass = document.getElementById("passInput").value;
+  if (namn == "David" && pass == "davble") {
+    window.location = "david.html";
+  }
+  else if (namn == "Hanna" && pass == "hanble") {
+    window.location = "hanna.html";
+  }
+  else if (namn == "Bästa föräldrarna" && pass == "päron") {
+    window.location = "morochfar.html";
+  }
+  else {
+    alert("Fel namn eller lösenord");
+  }
+}

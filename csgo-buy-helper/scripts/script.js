9a831ab5a6886403a2fe4973bf56998c0dca3cf5
blennster <mr.emil101@gmail.com>
2017-05-24 20:22:31 +0200

    Add files via upload

---
+++ csgo-buy-helper/scripts/script.js
@@ -0,0 +1,157 @@
+// JavaScript Document
+
+var T = "choiceT";
+var CT = "choiceCT";
+var moneyT, moneyCT;
+
+// Scrolla till toppen
+$(document).ready(function(){
+	 $("html,body").animate({scrollTop: 0}, 1000);
+});
+
+// Öppna sidomenyn
+$("#handle").click(function(){
+	var widthOffset = "-" + $("nav").css("width"); // Anpassar sig efter css stilen
+
+	if($("nav").css("left") == widthOffset){ // Om stängd
+		$("nav").css("left", "0px");
+	}
+	else{
+		$("nav").css("left", widthOffset);
+	}
+});
+
+$("#funding").keyup(function(){
+	var inputText = $("#funding").val();
+	var money = parseInt(inputText);
+	if(inputText != "" && !isNaN(money)){
+		buy(money);
+		$("#alternatives").css("opacity", "1"); // Set Visible
+	}else{
+		$("#alternatives").css("opacity", "0");
+	}
+});
+
+// Updatera reslutat
+$(".funding-alts input").change(function(){
+	$("#funding").trigger("keyup");
+});
+
+function buy(money){
+	clear();
+	moneyT = money;
+	moneyCT = money;
+	var firstRound = $("#first").prop("checked");
+	var winFirst = $("#round").prop("checked");
+	var troll = $("#troll").prop("checked");
+	var teamAWP = $("#awp").prop("checked");
+	if(money >= 8000)
+	{
+		if(teamAWP){
+			fullBuy();
+		}
+		else if(troll){
+			moneyT = buyM249(moneyT, T);
+			moneyCT = buyM249(moneyCT, CT);
+			buyHelm();
+			buyPistols();
+		}
+		else if(!troll){
+			buyHelm();
+			moneyT = buyAWP(moneyT, T);
+			moneyT = buyP250(moneyT, T);
+			buyGrenades(T);
+			moneyCT = buyAWP(moneyCT, CT);
+			moneyCT = buyP250(moneyCT, CT);
+			moneyCT = buyDefuser(moneyCT, CT);
+			moneyCT = buyIncen(moneyCT, CT);
+			moneyCT = buyFlash(moneyCT, CT);
+			moneyCT = buyHE(moneyCT, CT);
+			moneyCT = buySmoke(moneyCT, CT);
+		}
+	}
+	else if(money >= 5500){
+		fullBuy();
+	}
+	else if(money >= 5100)
+	{
+		fullBuy();
+		$("#CT .def").text("No");
+		moneyCT += 400;
+	}
+	else if(money >= 3000){
+		if(winFirst){
+			buyHelm();
+			moneyT = buyUMP45(moneyT, T);
+			moneyCT = buyUMP45(moneyCT, CT);
+		}
+	}
+	else if(firstRound){
+		moneyT = buyVest(moneyT, T);
+		moneyCT = buyVest(moneyCT, CT);
+	}
+
+	$("#" + T + " .mon").text("$" + moneyT);
+	$("#" + CT + " .mon").text("$" + moneyCT);
+}
+
+function fullBuy(){
+	moneyT = buyAK(moneyT, T);
+	moneyCT = buyM4(moneyCT, CT);
+	moneyCT = buyDefuser(moneyCT, CT);
+	buyGrenades();
+	buyHelm();
+	buyPistols();
+}
+
+function buyPistols(){
+	buyGlock(0, T);
+	buyUSP(0, CT);
+}
+
+function buyHelm(){
+	moneyT = buyHelmet(moneyT, T);
+	moneyCT = buyHelmet(moneyCT, CT);
+}
+
+function buyGrenades(side){
+	if(side == T){
+		moneyT = buyFlashx2(moneyT, T);
+		moneyT = buyHE(moneyT, T);
+		moneyT = buySmoke(moneyT, T);
+	}
+	else if(side == CT){
+		moneyCT = buyFlashx2(moneyCT, CT);
+		moneyCT = buyHE(moneyCT, CT);
+		moneyCT = buySmoke(moneyCT, CT);
+	}
+	else{
+		moneyT = buyFlashx2(moneyT, T);
+		moneyT = buyHE(moneyT, T);
+		moneyT = buySmoke(moneyT, T);
+		moneyCT = buyFlashx2(moneyCT, CT);
+		moneyCT = buyHE(moneyCT, CT);
+		moneyCT = buySmoke(moneyCT, CT);
+	}
+}
+
+function clear(){
+	$("#" + CT + " .prim").text("");
+	buyUSP(0, CT);
+	$("#" + CT + " .arm").text("");
+	$("#" + CT + " .def").text("No");
+	$("#" + CT + " .flash").text("0");
+	$("#" + CT + " .he").text("0");
+	$("#" + CT + " .smoke").text("0");
+	$("#" + CT + " .incen").text("0");
+	$("#" + CT + " .mon").text("$0");
+
+	$("#" + T + " .prim").text("");
+	buyGlock(0, T);
+	$("#" + T + " .arm").text("");
+	$("#" + T + " .flash").text("0");
+	$("#" + T + " .he").text("0");
+	$("#" + T + " .smoke").text("0");
+	$("#" + T + " .molo").text("0");
+	$("#" + T + " .mon").text("$0");
+}
---
+++ csgo-buy-helper/scripts/weapons.js
@@ -0,0 +1,179 @@
+// JavaScript Document
+
+// Enbart vapen som används av spelare
+// Onropa en av dessa metoder korrekt så kommer den ändra href attributen och texten till lämpliga värden och subtrahera från pengarna
+
+// Non side specific
+
+// Primary
+// Rifle
+function buyAWP(money, choice){
+	$("#" + choice + " .prim").text("AWP");
+	$("#" + choice + " .prim").attr("href", "weapons/awp.html");
+	return 	money - 4750;
+}
+
+function buyScout(money, choice){
+	$("#" + choice + " .prim").text("SSG 08");
+	$("#" + choice + " .prim").attr("href", "weapons/scout.html");
+	return 	money - 3100;
+}
+
+// SMG
+function buyUMP45(money, choice){
+	$("#" + choice + " .prim").text("UMP45");
+	$("#" + choice + " .prim").attr("href", "weapons/UMP45.html");
+	return 	money - 1200;
+}
+
+//Pistol
+function buyP250(money, choice){
+	$("#" + choice + " .sec").text("P250");
+	$("#" + choice + " .sec").attr("href", "weapons/p250.html");
+	return 	money - 300;
+}
+
+function buyDual(money, choice){
+	$("#" + choice + " .sec").text("Dual Berettas");
+	$("#" + choice + " .sec").attr("href", "weapons/dual.html");
+	return 	money - 500;
+}
+
+function buyDeagle(money, choice){
+	$("#" + choice + " .sec").text("Desert Eagle");
+	$("#" + choice + " .sec").attr("href", "weapons/deagle.html");
+	return 	money - 700;
+}
+
+//Heavy
+function buyM249(money, choice){
+	$("#" + choice + " .prim").text("M249");
+	$("#" + choice + " .prim").attr("href", "weapons/m249.html");
+	return 	money - 5200;
+}
+
+//Items
+function buyHelmet(money, choice){
+	$("#" + choice + " .arm").text("Helmet");
+	$("#" + choice + " .arms").attr("href", "weapons/armor.html");
+	return 	money - 1000;
+}
+function buyVest(money, choice){
+	$("#" + choice + " .arm").text("Vest");
+	$("#" + choice + " .arms").attr("href", "weapons/armor.html");
+	return 	money - 650;
+}
+function buyDefuser(money, choice){
+	$("#" + choice + " .def").text("Yes");
+	$("#" + choice + " .def").attr("href", "items/defuser.html");
+	return 	money - 400;
+}
+
+// Grenades
+function buyHE(money, choice){
+	$("#" + choice + " .he").text("1");
+	return 	money - 300;
+}
+function buyFlash(money, choice){
+	$("#" + choice + " .flash").text("1");
+	return 	money - 200;
+}
+function buyFlashx2(money, choice){
+	$("#" + choice + " .flash").text("2");
+	return 	money - 400;
+}
+function buySmoke(money, choice){
+	$("#" + choice + " .smoke").text("1");
+	return 	money - 300;
+}
+function buyMolo(money, choice){
+	$("#" + choice + " .molo").text("1");
+	return 	money - 400;
+}
+function buyIncen(money, choice){
+	$("#" + choice + " .incen").text("1");
+	return 	money - 600;
+}
+
+// Terrorist Weapons
+
+// Primary
+//Rifle
+function buyAK(money, choice){
+	$("#" + choice + " .prim").text("AK47");
+	$("#" + choice + " .prim").attr("href", "weapons/ak47.html");
+	return 	money - 2700;
+}
+
+function buyGalil(money, choice){
+	$("#" + choice + " .prim").text("Galil");
+	$("#" + choice + " .prim").attr("href", "weapons/galil.html");
+	return 	money - 2000;
+}
+
+function buySG553(money, choice){
+	$("#" + choice + " .prim").text("SG553");
+	$("#" + choice + " .prim").attr("href", "weapons/sg553.html");
+	return 	money - 3000;
+}
+
+function buyG3SG1(money, choice){
+	$("#" + choice + " .prim").text("G3SG1");
+	$("#" + choice + " .prim").attr("href", "weapons/g3sg1.html");
+	return 	money - 5000;
+}
+
+//Pistol
+function buyTec9(money, choice){
+	$("#" + choice + " .sec").text("Tec-9");
+	$("#" + choice + " .sec").attr("href", "weapons/tec9.html");
+	return 	money - 500;
+}
+
+function buyGlock(money, choice){
+	$("#" + choice + " .sec").text("Glock");
+	$("#" + choice + " .sec").attr("href", "weapons/glock.html");
+	return 	money - 500;
+}
+
+
+// Counter Terrorist weapons
+
+// Primary
+// Rifle
+function buyM4(money, choice){
+	$("#" + choice + " .prim").text("M4");
+	$("#" + choice + " .prim").attr("href", "weapons/m4.html");
+	return 	money - 3100;
+}
+
+function buyFamas(money, choice){
+	$("#" + choice + " .prim").text("Famas");
+	$("#" + choice + " .prim").attr("href", "weapons/famas.html");
+	return 	money - 2250;
+}
+
+function buyAUG(money, choice){
+	$("#" + choice + " .prim").text("Aug");
+	$("#" + choice + " .prim").attr("href", "weapons/aug.html");
+	return 	money - 3500;
+}
+
+function buySCAR20(money, choice){
+	$("#" + choice + " .prim").text("SCAR20");
+	$("#" + choice + " .prim").attr("href", "weapons/scar20.html");
+	return 	money - 3100;
+}
+
+//Pistol
+function buyFiveSeven(money, choice){
+	$("#" + choice + " .sec").text("Five SeveN");
+	$("#" + choice + " .sec").attr("href", "weapons/fiveseven.html");
+	return 	money - 500;
+}
+
+function buyUSP(money, choice){
+	$("#" + choice + " .sec").text("USP");
+	$("#" + choice + " .sec").attr("href", "weapons/usp.html");
+	return 	money;
+}
